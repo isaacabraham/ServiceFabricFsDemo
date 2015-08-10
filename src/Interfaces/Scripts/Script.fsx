8d252b86056de47c8ba9bde4ec12fcc0c6170581
Isaac Abraham <isaac.abraham@gmail.com>
2015-08-10 11:12:23 +0100

    Clean script.

--- src/Interfaces/Scripts/Script.fsx
+++ src/Interfaces/Scripts/Script.fsx
@@ -4,17 +4,6 @@ open Microsoft.ServiceFabric.Actors
 open ServiceFabricDemo
 open System
 
-// Download Service Fabric SDK and VS2015
-// Convert from nuget
-// Create console app
-// Create class library
-// Copy across package root folder
-// Create actor
-// Create interface
-// Target 64 bit (not AnyCPU) via Configuration Manager, .NET 4.5.1
-// Interfaces must have NAMED arguments!
-// Reference interface via assembly, NOT code.
-
 let cat = ActorProxy.Create<ICat>(ActorId "Blackie", "fabric:/CatActors")
 
 async {
